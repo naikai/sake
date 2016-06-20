e843a68d8e57d8d22f5acac2befb1e4d028dfb6a
Ray Ho <naikai@gmail.com>
2016-06-20 15:32:39 -0400

    Working version for corrplot with warning message. The problem is from using reactive expression to pass variable to modules.

--- R/corModule.R
+++ R/corModule.R
@@ -15,13 +15,14 @@ featureUI <- function(id) {
                ),
                fluidRow(
                  bsModal(ns("modalExample"), "Warning! Your sample size if above 200, it will take longer than usual. Are you sure you want to continue?",
-                         ns("runSamCor"), size = "small",
+                         trigger = "", size = "small",
                          actionButton(ns("cor_forcego_yes"), 'Yes'),
                          actionButton(ns("cor_forcego_no"), 'No')
                          )
                ),
                fluidRow(
-                 column(width=12, corModuleUI(ns("sample")))
+                 # column(width=12, corModuleUI(ns("sample")))
+                 column(width=12,  plotOutput(ns('sampleCorPlot')))
                ),
                fluidRow(
                  column(width=12, bsAlert("cor_alert"))
@@ -51,7 +52,20 @@ featureUI <- function(id) {
 #' @export
 feature <- function(input, output, session, data){
   go <- reactiveValues(run=FALSE)
-  # runsam <- reactiveValues(num=0)
+
+  observeEvent(input$runSamCor, {
+    # if(ncol(data()) > 200){
+    if(nrow(data()) > 200){
+      print('greater than 200')
+      toggleModal(session, "modalExample", toggle = "open")
+      # createAlert(session, "cor_alert", "cor_alert1", title = "Warning", style = "warning",
+      #             content = "Data contains more than 200 samples, It might take longer than usual to run. <br>
+      #             Please make sure you are aware of this and still want to run",
+      #             append = TRUE)
+    }else{
+      go$run <- TRUE
+    }
+  })
 
   observeEvent(input$cor_forcego_yes, {
     print('force go yes')
@@ -63,32 +77,51 @@ feature <- function(input, output, session, data){
     toggleModal(session, "modalExample", toggle = "close")
   })
 
-  observeEvent(input$runSamCor, {
-    # if(ncol(data()) > 200){
-    if(nrow(data()) > 200){
-      print('greater than 200')
-      # createAlert(session, "cor_alert", "cor_alert1", title = "Warning", style = "warning",
-      #             content = "Data contains more than 200 samples, It might take longer than usual to run. <br>
-      #             Please make sure you are aware of this and still want to run",
-      #             append = TRUE)
-    }else{
-      go$run <- TRUE
-    }
-  })
-
   observe({
     if(go$run == FALSE) return()
 
     isolate({
       print('run')
       print(paste('gorun:', go$run))
-      tl_cex <- reactive(input$cor_sam_lab_cex)
-      number_cex <- reactive(input$cor_num_lab_cex)
-      type <- reactive(input$cor_type)
-      callModule(corModule, "sample", reactive({ data() }),
-                 tl_cex = tl_cex,
-                 number_cex = number_cex,
-                 type = type, run = reactive({go$run}) )
+
+      # tl_cex <- reactive(input$cor_sam_lab_cex)
+      # number_cex <- reactive(input$cor_num_lab_cex)
+      # type <- reactive(input$cor_type)
+      tl_cex <- input$cor_sam_lab_cex
+      number_cex <- input$cor_num_lab_cex
+      type <- input$cor_type
+
+      # callModule(corModule, "sample", reactive({ data() }),
+      #            tl_cex = tl_cex,
+      #            number_cex = number_cex,
+      #            type = type)
+
+      diag <- FALSE
+      output$sampleCorPlot <- renderPlot({
+        if(type == "full"){
+          diag = TRUE
+        }
+        n <- 2
+        withProgress(message = 'Calculating correlation', value = 0, {
+          incProgress(1/n, detail = "Takes around 10 seconds")
+          M <- cor(data())
+          p.mat <- cor_mtest(data())
+        })
+        col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA", "#79AEDD", "#FFFFFF", "#2E9988", "#2B4444"))
+
+        withProgress(message = 'Plotting..', value = 0, {
+          incProgress(1/n, detail = "Takes around 10-20 seconds")
+          corrplot(M, method="color", col=rev(col(200)),
+                   type=type, order="hclust",
+                   addCoef.col = "black", # Add coefficient of correlation
+                   tl.col="black", tl.srt=45, tl.cex=tl_cex, #Text label color and rotation
+                   number.cex = number_cex,
+                   p.mat = p.mat, sig.level = 0.01, insig = "blank",
+                   diag=diag
+          )
+        })
+      }, height=500)
+
       go$run <- FALSE
     })
   })
@@ -112,42 +145,33 @@ corModuleUI <- function(id) {
 
 #' @export
 corModule <- function(input, output, session, data,
-                      tl_cex = 0.2, number_cex = 0.01, type = "upper",
-                      diag = FALSE, height = 480, run=FALSE){
+                      tl_cex, number_cex, type,
+                      diag = FALSE, height = 480){
   if(type() == "full"){
     diag = TRUE
   }
   print('running cormodule .. ')
-  print('corModule run:', run())
 
-  observe({
-    if(run() == TRUE){
-      print('inside observe run:', run())
+  output$sampleCorPlot <- renderPlot({
+    n <- 2
+    withProgress(message = 'Calculating correlation', value = 0, {
+      incProgress(1/n, detail = "Takes around 10 seconds")
+      M <- cor(data())
+      p.mat <- cor_mtest(data())
+    })
+    col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA", "#79AEDD", "#FFFFFF", "#2E9988", "#2B4444"))
 
-      isolate(
-        output$sampleCorPlot <- renderPlot({
-          n <- 2
-          withProgress(message = 'Calculating correlation', value = 0, {
-            incProgress(1/n, detail = "Takes around 10 seconds")
-            M <- cor(data())
-            p.mat <- cor_mtest(data())
-          })
-          col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA", "#79AEDD", "#FFFFFF", "#2E9988", "#2B4444"))
-
-          withProgress(message = 'Plotting..', value = 0, {
-            incProgress(1/n, detail = "Takes around 10-20 seconds")
-            corrplot(M, method="color", col=rev(col(200)),
-                     type=type(), order="hclust",
-                     addCoef.col = "black", # Add coefficient of correlation
-                     tl.col="black", tl.srt=45, tl.cex=tl_cex(), #Text label color and rotation
-                     number.cex = number_cex(),
-                     p.mat = p.mat, sig.level = 0.01, insig = "blank",
-                     diag=diag
-            )
-          })
-        }, height=height)
+    withProgress(message = 'Plotting..', value = 0, {
+      incProgress(1/n, detail = "Takes around 10-20 seconds")
+      corrplot(M, method="color", col=rev(col(200)),
+               type=type(), order="hclust",
+               addCoef.col = "black", # Add coefficient of correlation
+               tl.col="black", tl.srt=45, tl.cex=tl_cex(), #Text label color and rotation
+               number.cex = number_cex(),
+               p.mat = p.mat, sig.level = 0.01, insig = "blank",
+               diag=diag
       )
-    }
-  })
+    })
+  }, height=height)
 }
 
